8a3703041d58ebf354ebdf699b4f433de20957cc
hungtxgch17308 <hungtxgch17308@fpt.edu.com>
2020-01-03 13:18:02 +0700

    1

--- index.php
+++ index.php
@@ -41,12 +41,12 @@
 				<form class="login100-form validate-form p-b-33 p-t-5"  action="managing.php" method="POST">
 
 					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" type="text" name="username" placeholder="User name">
+						<input class="input100" type="text" name="aduser" placeholder="User name">
 						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="pass" placeholder="Password">
+						<input class="input100" type="password" name="adpass" placeholder="Password">
 						<span class="focus-input100" data-placeholder="&#xe80f;"></span>
 					</div>
 
